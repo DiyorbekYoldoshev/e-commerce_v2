10d741bef645c6de5a38d94fb5bf1fb79b4b8e29
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2026-01-15 15:33:22 +0500

    Initial commit

--- order_modul/apps.py
+++ order_modul/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class OrderModulConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    name = 'order_modul'
