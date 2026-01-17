2367a27ad5e191aeee3e830888a622ddfa644351
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2026-01-17 15:46:04 +0500

    Initial commit

---
+++ order_modul/serializers/__init__.py
@@ -0,0 +1,11 @@
+from .coupon import CouponSerializer
+from .order_item import OrderItemSerializer, OrderItemCreateSerializer
+from .order import (
+    OrderListSerializer,
+    OrderDetailSerializer,
+    OrderCreateSerializer,
+)
+from .installment import (
+    InstallmentPlanSerializer,
+    InstallmentPaymentSerializer,
+)
