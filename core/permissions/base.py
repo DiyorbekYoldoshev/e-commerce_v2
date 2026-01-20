7f1577f7231f05c69fb9d2397e7533b816547653
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2026-01-20 23:46:51 +0500

    permission base

--- core/permissions/base.py
+++ core/permissions/base.py
@@ -1,20 +1,46 @@
+# permissions/base.py
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthenticatedAndActive(BasePermission):
+    """
+    Login qilgan va aktiv userlar uchun
+    """
 
-    """
-    Login qilgan va activ userlar
-    """
     def has_permission(self, request, view):
         return bool(
             request.user
             and request.user.is_authenticated
             and request.user.is_active
+            and not request.user.is_deleted  # qo'shimcha tekshiruv
         )
 
-class ReadyOnly(BasePermission):
 
-    """Faqat o'qish uchun"""
+class ReadOnly(BasePermission):
+    """
+    Faqat o'qish (GET, HEAD, OPTIONS) uchun
+    """
+
     def has_permission(self, request, view):
-        return request.method == SAFE_METHODS
+        return request.method in SAFE_METHODS
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    O'qish hammaga, tahrirlash faqat egasiga
+    """
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        # obj.user bor bo'lsa
+        if hasattr(obj, 'user'):
+            return obj.user == request.user
+
+        return False
