4052d83663cec05a2e413cf52ab74b465dbb9c16
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2026-01-21 22:49:15 +0500

    permission base

---
+++ users/views/user.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from ..serializers.user import UserSerializer
+from ..models.user import User
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class =
