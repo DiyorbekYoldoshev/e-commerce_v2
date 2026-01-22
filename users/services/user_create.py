ebaf9a2fab554b4d926a0eaef477f3681945efc0
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2026-01-22 20:34:57 +0500

    services opt

--- users/services/user_create.py
+++ users/services/user_create.py
@@ -1,17 +1,54 @@
-from django.contrib.auth import get_user_model
-from django.db import transaction
-
-User = get_user_model()
+from django.contrib.auth.password_validation import validate_password
+from django.db import transaction, IntegrityError
+from ..models.profile import Profile
+from ..models.user import User
+from rest_framework.validators import ValidationError
 
 @transaction.atomic
-def create_user(*,email:str,password:str,**extra_fields) -> User:
+def create_user(
+        *,
+        email:str,
+        password:str,
+        first_name:str | None=None,
+        last_name:str | None=None,
+        **extra_fields,
+    ) -> User:
 
-    email = User.objects.normalize_email(email)
-
-    user = User(
-        email=email,
-        **extra_fields
-    )
-    user.set_password(password)
-    user.save()
+    if not email:
+        raise ValidationError(
+            {'email':"Email bo'lishi kerak"}
+        )
+    email = User.objects.normalize_email(email=email).lower().strip()
+    validate_password(password=password)
+    try:
+        user = User.objects.create_user(
+            email=email,
+            password=password,
+            first_name=first_name or "",
+            last_name=last_name or "",
+            **extra_fields
+        )
+    except IntegrityError:
+        raise ValidationError(
+            {'email':"Bu email oldin ro'yxatdan o'tgan"}
+        )
     return user
+
+@transaction.atomic
+def create_superuser(
+        *,
+        email:str,
+        password:str,
+        **extra_fields
+    ) -> User:
+    extra_fields.setdefault('is_staff',True)
+    extra_fields.setdefault('is_superuser',True)
+    extra_fields.setdefault('is_active',True)
+    return create_superuser(email=email,password=password,**extra_fields)
+
+
+
+
+
+
+
