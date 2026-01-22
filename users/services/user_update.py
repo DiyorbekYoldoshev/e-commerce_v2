86ea766218999915cbbe2997072f2d2cf77b3c09
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2026-01-22 20:34:49 +0500

    services opt

--- users/services/user_update.py
+++ users/services/user_update.py
@@ -1,10 +1,21 @@
-from django.contrib.auth import get_user_model
+from django.db import transaction
+from users.models.profile import Profile
 
-User = get_user_model()
+@transaction.atomic
 
-def update_user(*,user:User,data:dict) -> User:
+def update_me(*,user,data:dict):
 
-    for field, value in data.items():
-        setattr(user,field,value)
-    user.save(update_fields=data.keys())
-    return user
+    user_fields = ['first_name','last_name','gender']
+    for f in user_fields:
+        if f in data:
+            setattr(user,f,data[f])
+    user.save(update_fields=[f for f in user_fields if f in data])
+
+    profile_fields = ['phone','bio']
+    prof_data = {f:data[f] for f in profile_fields if f in data}
+    if prof_data:
+        profile,_ = Profile.objects.get_or_create(user=user)
+        for f, v in prof_data.items():
+            setattr(profile,f,v)
+        profile.save(update_fields=list(prof_data.keys()))
+        return user_fields
