f26c92d7f207b51a96fd5c3aeac42fc593176d21
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2026-02-01 22:07:52 +0500

    initial commit

--- seller_modul/urls/urls_seller.py
+++ seller_modul/urls/urls_seller.py
@@ -1,6 +1,9 @@
 # seller_modul/urls.py
 
+app_name = 'seller_modul'
+
 from rest_framework.routers import DefaultRouter
+from django.urls import include, path
 
 from seller_modul.views.views_seller import SellerViewSet
 from seller_modul.views.views_seller_request import SellerRequestViewSet
@@ -9,25 +12,8 @@ router = DefaultRouter()
 router.register(r"sellers", SellerViewSet, basename="seller")
 router.register(r"seller-requests", SellerRequestViewSet, basename="sellerrequest")
 
-from django.urls import path, include
-
-from users.views.profile import ProfileView, MeView
-
-from users.views.auth.login import LoginView
-from users.views.auth.register import RegisterView
-from users.views.auth.password import ChangePasswordView
-
 urlpatterns = [
-    # auth
-    path("auth/login/", LoginView.as_view()),
-    path("auth/register/", RegisterView.as_view()),
-    path("auth/password/change/", ChangePasswordView.as_view()),
-
-    # me/profile
-    path("me/", MeView.as_view()),
-    path("profile/", ProfileView.as_view()),
-
-    # router urls
+    # router-provided endpoints (list, retrieve, custom actions...)
     path("", include(router.urls)),
 ]
 
--- users/urls/misc.py
+++ users/urls/misc.py
@@ -1,3 +1,5 @@
+app_name = 'users'
+
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,15 +14,15 @@ from users.views.auth.password import ChangePasswordView
 
 urlpatterns = [
     # auth
-    path("auth/login/", LoginView.as_view()),
-    path("auth/register/", RegisterView.as_view()),
-    path("auth/password/change/", ChangePasswordView.as_view()),
+    path("auth/login/", LoginView.as_view(), name='login'),
+    path("auth/register/", RegisterView.as_view(), name='register'),
+    path("auth/password/change/", ChangePasswordView.as_view(), name='password_change'),
 
     # jwt token endpoints
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # me/profile
-    path("me/", MeView.as_view()),
-    path("profile/", ProfileView.as_view()),
+    path("me/", MeView.as_view(), name='me'),
+    path("profile/", ProfileView.as_view(), name='profile'),
 ]
