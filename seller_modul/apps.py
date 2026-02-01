1d81fc52be5dcf94e2b9d066e3cfe64c7ab106a8
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2026-02-01 21:54:50 +0500

    initial commit

--- seller_modul/apps.py
+++ seller_modul/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SellerModulConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'seller_modul'
+
+    def ready(self):
+        # import signals to ensure they are registered
+        from . import signals  # noqa: F401
---
+++ seller_modul/views/__init__.py
@@ -0,0 +1,2 @@
+from .views_seller import *
+from .views_seller_request import *
